6dd79c997ee78a99adadca637adc6b9e1e1177ac
Jose Pieters <jose.pieters@Hotmail.com>
2020-03-30 09:59:07 -0400

    Base made still working on it

---
+++ portfolio/src/components/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Jose Pieters</h1>
+    <img src= "https://www.w3schools.com/w3css/img_avatar3.png" alt="image"></img>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
+      varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
+      Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
+      imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
+      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare elit.
+      Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
+      consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+      fames ac ante ipsum primis in faucibus.
+    </p>
+  </div>
+);
+
+export default Home;
