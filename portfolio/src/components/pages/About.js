fd03fabecafcc12960c2a457821d9dfb378545b9
Jose Pieters <jose.pieters@Hotmail.com>
2020-04-07 11:05:20 -0400

    update on about page

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,15 +1,45 @@
 import React from "react";
+import { Jumbotron, Container, Row, Col, Card } from "react-bootstrap";
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
+    <Jumbotron>
+      <h3 className="intro">Introduction (Random text will be edited)</h3>
+      <Container>
+        <p>
+          Knowledge nay estimable questions repulsive daughters boy. Solicitude
+          gay way unaffected expression for. His mistress ladyship required off
+          horrible disposed rejoiced. Unpleasing pianoforte unreserved as oh he
+          unpleasant no inquietude insipidity. Advantages can discretion
+          possession add favourable cultivated admiration far. Why rather assure
+          how esteem end hunted nearer and before. By an truth after heard going
+          early given he. Charmed to it excited females whether at examine. Him
+          abilities suffering may are yet dependent.
+        </p>
+
+        <p>
+          Knowledge nay estimable questions repulsive daughters boy. Solicitude
+          gay way unaffected expression for. His mistress ladyship required off
+          horrible disposed rejoiced. Unpleasing pianoforte unreserved as oh he
+          unpleasant no inquietude insipidity. Advantages can discretion
+          possession add favourable cultivated admiration far. Why rather assure
+          how esteem end hunted nearer and before. By an truth after heard going
+          early given he. Charmed to it excited females whether at examine. Him
+          abilities suffering may are yet dependent.
+        </p>
+        <p>
+          Knowledge nay estimable questions repulsive daughters boy. Solicitude
+          gay way unaffected expression for. His mistress ladyship required off
+          horrible disposed rejoiced. Unpleasing pianoforte unreserved as oh he
+          unpleasant no inquietude insipidity. Advantages can discretion
+          possession add favourable cultivated admiration far. Why rather assure
+          how esteem end hunted nearer and before. By an truth after heard going
+          early given he. Charmed to it excited females whether at examine. Him
+          abilities suffering may are yet dependent.
+        </p>
+        <p>Coffee? My favourite beverage! I am no picky eater.</p>
+      </Container>
+    </Jumbotron>
   </div>
 );
 
