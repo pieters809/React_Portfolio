03b2d2b2a82d4101041f95b671151b84af711b8a
Jose Pieters <jose.pieters@Hotmail.com>
2020-04-07 11:04:39 -0400

    update on home page

--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { Container, Row, Col, Image, Jumbotron } from "react-bootstrap";
+
 
 const Home = () => (
   <div>
-    <h1>Jose Pieters</h1>
-    <img src= "https://www.w3schools.com/w3css/img_avatar3.png" alt="image"></img>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-      varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-      Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-      imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
-      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare elit.
-      Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-      consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-      fames ac ante ipsum primis in faucibus.
-    </p>
+
+<Row>
+        <Col className="text-center">
+          <Jumbotron>
+            <Container className="justify-content-md-center">
+              <Row>
+                <Col>
+                  <h1 class="text-center">Jose Pieters</h1>
+                  <img src= "https://www.w3schools.com/w3css/img_avatar3.png" alt="image"></img>
+                  <h4>Welcome to my portfolio</h4>
+                </Col>
+              </Row>
+            </Container>
+          </Jumbotron>
+        </Col>
+      </Row>
   </div>
 );
 
