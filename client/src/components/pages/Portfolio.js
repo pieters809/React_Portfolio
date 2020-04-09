d5bc5d14797101d49a6def5ba50317cb88061af3
Jose Pieters <jose.pieters@Hotmail.com>
2020-04-08 20:30:25 -0400

    Portfolio layed out

--- client/src/components/pages/Portfolio.js
+++ client/src/components/pages/Portfolio.js
@@ -1,27 +1,101 @@
 import React from "react";
-import { Jumbotron, Container} from "react-bootstrap";
+import { Jumbotron, Container, Card, Button, Row, Col } from "react-bootstrap";
 
 import "./pages.css";
 
-
 const Portfolio = () => (
   <div>
-  <Jumbotron>
-    <h3 className="intro">Portfolio</h3>
-    <Container>
-      <p>
-        Knowledge nay estimable questions repulsive daughters boy. Solicitude
-        gay way unaffected expression for. His mistress ladyship required off
-        horrible disposed rejoiced. Unpleasing pianoforte unreserved as oh he
-        unpleasant no inquietude insipidity. Advantages can discretion
-        possession add favourable cultivated admiration far. Why rather assure
-        how esteem end hunted nearer and before. By an truth after heard going
-        early given he. Charmed to it excited females whether at examine. Him
-        abilities suffering may are yet dependent.
-      </p>
-    </Container>
-  </Jumbotron>
-</div>
+    <Jumbotron>
+      <h3 className="intro">Portfolio</h3>
+      <Container>
+        {/* Row of cards 1  */}
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="auto">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="auto">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        {/* Row of cards 2 */}
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="auto">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md="auto">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+       
+      </Container>
+    </Jumbotron>
+  </div>
 );
 
 export default Portfolio;
